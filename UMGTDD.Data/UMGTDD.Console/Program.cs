944d02e9578a105ba35b7ec9d5bf67e2fd0b05a7
JHitchhiker <jai.holloway@hotmail.co.za>
2017-04-05 17:53:02 +0100

    Console App working

--- UMGTDD.Data/UMGTDD.Console/Program.cs
+++ UMGTDD.Data/UMGTDD.Console/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UMGTDD.Service;
 
-namespace UMGTDD.Console
+namespace UMGTDD.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please provide a Partner and Effective Date for a list of available Tracks");
+            var command = "";
+            var inputHandler = new InputHandler();
+            while (command != "exit")
+            {
+                
+                command = Console.ReadLine();
+                try
+                {
+                    Console.Clear();
+                    var results = inputHandler.HandleCommand(command);
+
+                    foreach (var result in results)
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Input Supplied. Please try again - [PARTNER] [1st Jan 2012]");
+                }
+                
+            }
         }
     }
 }
